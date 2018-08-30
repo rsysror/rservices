90bfad4ac7f5a05a8279c89a3bb12bf4a589a5f1
Puja Gupta <rorruby2@gmail.com>
2018-08-30 17:38:42 +0530

    add sign_up popup

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,34 @@
+$(document).ready(function() { 
+  
+  $("form#new_user").submit(function(e) {
+    e.preventDefault(); 
+    var form = $(this);
+    var url = form.attr('action');
+
+    $.ajax({
+      type: "POST",
+      url: url,
+      dataType: "json",
+      data: form.serialize(), // serializes the form's elements.
+      success: function(data)
+      { 
+        window.location.href = "/";
+      },
+      error: function(err)
+      { 
+        $('#display_errors').css('display','block');
+        $("#display_errors").html(err.responseText);
+      }
+    });
+  });
+
+  $("#close_button").click( function()
+    {
+      console.log("****************")
+      $(this).find('form')[0].reset();
+      
+      
+    }
+  );
+
+});

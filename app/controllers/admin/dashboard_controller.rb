b892edbf577aed16ab0e5e0f7497cf4088b7df83
Sanjay Yadav1 <sanjay.yadav1@rsystems.com>
2018-08-31 14:34:30 +0530

    Implemented header and logout.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < AdminController
 
 	def index
-
+		@totalServices = Service.all.where(parent_id: nil).count
+		@allUsers = User.all.count
+		# byebug
+		@cities = City.all.count
 	end	
 end

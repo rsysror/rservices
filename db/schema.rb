1e46326ef868b52a7e4a889eb6e581c517bf5feb
amrendra <amrendra510@gmail.com>
2018-09-03 15:08:09 +0530

    adding service request number column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_084414) do
+ActiveRecord::Schema.define(version: 2018_09_03_084311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_084414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "portfolio_id"
+    t.string "service_request_number"
   end
 
   create_table "services", force: :cascade do |t|
@@ -100,6 +101,13 @@ ActiveRecord::Schema.define(version: 2018_08_31_084414) do
 
   create_table "statuses", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sub_services", force: :cascade do |t|
+    t.string "name"
+    t.integer "service_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

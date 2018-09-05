de647b895dfcff08ed9e00b29c330d7580d8df3b
amrendra <amrendra510@gmail.com>
2018-09-05 09:53:03 +0530

    adding environment file

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV[SENDGRID_USERNAME],
-  :password       => ENV[SENDGRID_PASSWORD],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com'
 }

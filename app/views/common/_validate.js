ecb70aed4d6196fe628de07748d1702276109721
Sanjay Yadav1 <sanjay.yadav1@rsystems.com>
2018-09-05 14:24:35 +0530

    Impemented Proper Error message on client-side validate.

--- app/views/common/_validate.js
+++ app/views/common/_validate.js
@@ -54,11 +54,12 @@ $("#new_user").validate({
 
 
 // Validating service on cient side.
-$("#new_service").validate({
+$("#service_form").validate({
   //error place
   errorPlacement: function (error, element) {
     error.insertBefore(element);
   },
+  debug: true,
 //adding rule
   rules: {
     // username is required with max of 20 and min of 6
@@ -73,12 +74,12 @@ $("#new_service").validate({
   },
   // error messages
   messages: {
-    name:{
+    "service[name]":{
       required: "Service name is required!",
-      maxlength: "Service name must be less than 110.",
+      maxlength: "Service name must be less than 115.",
       minlength: "Service name must be more than 3."
     },
-    city_ids:{
+    "service[city_ids][]":{
       required: "At leaset 1 city should be selected!",
     }
   }

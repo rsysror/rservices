d89b2fc614d7990b8b2cfce5ec34f7f861cb4fe9
Puja Gupta <rorruby2@gmail.com>
2018-09-10 16:58:55 +0530

    add photo for service and implement theme for service in admin

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,6 @@
 class Service < ApplicationRecord
+  mount_uploader :photo, ImageUploader
+
   has_and_belongs_to_many :cities
   has_many :sub_services, class_name: "Service", foreign_key: "parent_id", dependent: :destroy
   belongs_to :service, class_name: "Service",  foreign_key: "parent_id",optional: true
@@ -10,10 +12,10 @@ class Service < ApplicationRecord
   validates :name, length: { in: 3..110, message: 'There must be 3 to 110 characters only!' }
   validates :city_ids, presence: {message: 'At leaset 1 city should be selected!'}, if: :is_parent_id_nil?
   #scope methods
-	scope :get_services, -> { where("parent_id IS NULL") }
+  scope :get_services, -> { where("parent_id IS NULL") }
 
   def is_parent_id_nil?
     self.parent_id == nil
   end
-	
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_050137) do
+ActiveRecord::Schema.define(version: 2018_09_10_055143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2018_09_07_050137) do
     t.integer "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "states", force: :cascade do |t|

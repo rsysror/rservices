b8c80e2966ec6a546893ec7408eae30b305ea0d2
Sanjay Yadav1 <sanjay.yadav1@rsystems.com>
2018-09-10 18:52:22 +0530

    Implemented Searching on home page for services based on location.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
 	before_action :authenticate_user!, except: [:index]
 	
   def index
+  	city = params[:post] ? params[:post][:city_id] : ''
+  	# service = params[:post] ? params[:post][:service] : ''
+  	if !params[:post].nil?
+  		@services = City.find(city).services
+  	else
+  		@services = Service.all
+  	end
+  	# @services = Service.search(city,service)
   end
 
   def dashboard

7b4b2921192a440edd8395cbc30725dae8b50694
amrendra <amrendra510@gmail.com>
2018-09-11 13:19:44 +0530

    code refactoring

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,7 +2,7 @@ module AdminHelper
 
 	def get_rating_data user
   	score = user.portfolio.feedbacks.present? ? actual_rating_count(user.portfolio) : 0
-  	render partial: "shared/show_rating" , locals: {request: user, score: score} 
+  	render_rating_partial(user, score)
   end
 
   def actual_rating_count portfolio
@@ -15,6 +15,15 @@ module AdminHelper
  	 	else
  	 		object.constantize.count
  	 	end
+ 	end
+
+ 	def render_rating_partial user, score
+ 		render partial: "shared/show_rating" , locals: {request: user, score: score}
+ 	end
+
+
+  def get_service_request_rating request
+ 	  (request.status.name == "Completed" and request.feedback.present?) ?  render_rating_partial(request, request.feedback.rating): "Not Yet Rated"
  	end
 
 end	

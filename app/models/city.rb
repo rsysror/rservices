78315b7f7b366ef6c00bd9cf5b293702137307a9
Amrendra <amrendra510@gmail.com>
2018-09-19 12:53:50 +0530

    changing associations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,8 +1,10 @@
 class City < ApplicationRecord
   #associations
-	has_and_belongs_to_many :services
+	# has_and_belongs_to_many :services
   has_many :addresses
   has_many :portfolio
+  has_many :services, through: :portfolio_services
+  has_many :portfolio_services
 	
   #scope methods
 	scope :details, proc{ |state_id| self.where(:state_id => state_id).order(name: :desc) if state_id.present? }
--- app/models/portfolio_service.rb
+++ app/models/portfolio_service.rb
@@ -1,5 +1,5 @@
 class PortfolioService < ApplicationRecord
   belongs_to :service
   belongs_to :portfolio
-
+  belongs_to :city
 end

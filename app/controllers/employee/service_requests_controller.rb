2d20e81ed39a0ddac0b56cf9344b5979949aa2c3
Sanjay Yadav1 <sanjay.yadav1@rsystems.com>
2018-09-19 18:59:12 +0530

    worked to implement accept reject for employees.

--- app/controllers/employee/service_requests_controller.rb
+++ app/controllers/employee/service_requests_controller.rb
@@ -1,7 +1,8 @@
 class Employee::ServiceRequestsController < EmployeesController
 
   def index
-    @service_requests = current_user.service_requests.includes(:service,:address,:status, :portfolio, :time_slot).ordered.paginate(:page => params[:page], :per_page => 5)
+    # @service_requests = current_user.service_requests.includes(:service,:address,:status, :portfolio, :time_slot).ordered.paginate(:page => params[:page], :per_page => 5)
+    @service_requests = current_user.assigned_service_requests.ordered.paginate(:page => params[:page], :per_page => 5)
   end  
 
 end
--- app/helpers/employee/service_requests_helper.rb
+++ app/helpers/employee/service_requests_helper.rb
@@ -1,2 +1,20 @@
 module Employee::ServiceRequestsHelper
+ 
+  def employ_action request
+    if ["Pending"].include?(request.status.try(:name))
+      (link_to 'Accept', employee_root_path(value: "accepted", id: request.id), method: :put, :remote => true, class: " btn btn-success btn-xs btn_margin") +
+      (link_to 'Reject', employee_root_path(value: "rejected", id:request.id ), method: :put, :remote => true, class: " btn btn-danger btn-xs" ) +
+      (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
+    elsif request.status.try(:name) == "Accepted"
+      (link_to 'Complete', employee_root_path(value: "completed", id: request.id), method: :put, :remote => true, class: " btn btn-success btn-xs btn_margin") +
+      (link_to 'In Complete', employee_root_path(value: "incompleted", id: request.id), method: :put, :remote => true, class: " btn btn-danger btn-xs") +
+      (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
+    elsif (request.status.try(:name) == "Completed")
+      request.feedback.present? ? add_rating_html_block(request) : "Not Yet Rated" 
+      (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
+    else
+      "-"
+    end
+  end
+  
 end

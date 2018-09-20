5260115364e0daa3bd0333bdf43671bd79330944
sandip mondal <sandip.mondal@rsystem.com>
2018-09-20 18:36:37 +0530

    remove comments link from employee

--- app/helpers/employee/service_requests_helper.rb
+++ app/helpers/employee/service_requests_helper.rb
@@ -3,11 +3,11 @@ module Employee::ServiceRequestsHelper
   def employee_action request
     if request.status == Status.inprogress.last
       (link_to 'Complete', accept_reject_employee_service_requests_path(value: "completed", id: request.id), method: :put, :remote => true, class: " btn btn-success btn-xs btn_margin") +
-      (link_to 'On hold', accept_reject_employee_service_requests_path(value: "onhold", id: request.id), method: :put, :remote => true, class: " btn btn-danger btn-xs") +
-      (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
+      (link_to 'On hold', accept_reject_employee_service_requests_path(value: "onhold", id: request.id), method: :put, :remote => true, class: " btn btn-danger btn-xs") 
+      # (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
     elsif (request.status == Status.completed.last )
       request.feedback.present? ? add_rating_html_block(request) : "Not Yet Rated" 
-      (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
+      # (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
     else
       "-"
     end

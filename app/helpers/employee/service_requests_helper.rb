0efac5461c180b5999cbee63d42e34f3623d942e
Sanjay Yadav1 <sanjay.yadav1@rsystems.com>
2018-09-20 13:15:36 +0530

    Implemented complete/on hold for employ.

--- app/helpers/employee/service_requests_helper.rb
+++ app/helpers/employee/service_requests_helper.rb
@@ -1,13 +1,9 @@
 module Employee::ServiceRequestsHelper
  
   def employ_action request
-    if ["Pending"].include?(request.status.try(:name))
-      (link_to 'Accept', employee_root_path(value: "accepted", id: request.id), method: :put, :remote => true, class: " btn btn-success btn-xs btn_margin") +
-      (link_to 'Reject', employee_root_path(value: "rejected", id:request.id ), method: :put, :remote => true, class: " btn btn-danger btn-xs" ) +
-      (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
-    elsif request.status.try(:name) == "Accepted"
-      (link_to 'Complete', employee_root_path(value: "completed", id: request.id), method: :put, :remote => true, class: " btn btn-success btn-xs btn_margin") +
-      (link_to 'In Complete', employee_root_path(value: "incompleted", id: request.id), method: :put, :remote => true, class: " btn btn-danger btn-xs") +
+    if request.status.try(:name) == "Accepted"
+      (link_to 'Complete', accept_reject_employee_service_requests_path(value: "completed", id: request.id), method: :put, :remote => true, class: " btn btn-success btn-xs btn_margin") +
+      (link_to 'On hold', accept_reject_employee_service_requests_path(value: "on-hold", id: request.id), method: :put, :remote => true, class: " btn btn-danger btn-xs") +
       (link_to 'Comment', open_comment_pop_up_path(id: request.id), method: :get, :remote => true, id: '#comment', class: " btn btn-info btn-xs btn_margin")
     elsif (request.status.try(:name) == "Completed")
       request.feedback.present? ? add_rating_html_block(request) : "Not Yet Rated" 

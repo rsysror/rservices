80c73629d516993268f23d50cc29d1819cb89f8f
Sanjay Yadav1 <sanjay.yadav1@rsystems.com>
2018-09-27 18:35:14 +0530

    Code refector for validation.

--- app/assets/javascripts/service_requests.js
+++ app/assets/javascripts/service_requests.js
@@ -7,10 +7,33 @@ $(document).on("turbolinks:load", function() {
          if ( this.value === 'Other Reason')
          {
            $("#comment-field").show();
+           validate_comment()
          }
          else
          {
            $("#comment-field").hide();
          }
     });
+
+    function validate_comment(){
+        $("#comment_form").validate({
+            errorPlacement: function (error, element) {
+              error.insertBefore(element);
+            },
+            rules: {
+              "service_request[comment]":{
+                required: true,
+                maxlength: 150,
+                minlength: 15
+              }
+            },
+            messages: {
+              "service_request[comment]":{
+                required: "Description is required!",
+                maxlength: "Maximum 150 characters are allowed!",
+                minlength: "Minimum 15 characters required!"
+              }
+            }
+        })
+    };
 });

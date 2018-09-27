3b669d50ad0e538ab5f53c3f5143cf6b5247e655
Sanjay Yadav1 <sanjay.yadav1@rsystems.com>
2018-09-27 16:30:57 +0530

    Removed complete validation of parting.

--- app/assets/javascripts/service_requests.js
+++ app/assets/javascripts/service_requests.js
@@ -1,3 +1,16 @@
 function rating_value_feed(selector,score){
     $('#'+ selector).raty({readOnly: true,  score: score });    
 }
+
+$(document).on("turbolinks:load", function() {
+	$(document).on('change', '#service_request_select_comment', function() {
+         if ( this.value === 'Other Reason')
+         {
+           $("#comment-field").show();
+         }
+         else
+         {
+           $("#comment-field").hide();
+         }
+    });
+});

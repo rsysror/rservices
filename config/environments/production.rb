0b4584f7677b6ebd24ad02b85a65ab724ed5ae56
Puja Gupta <rorruby2@gmail.com>
2018-10-08 16:39:58 +0530

    adding precompile true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,13 +29,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
-end
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
